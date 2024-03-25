334562e82d089c0fa63492a0274643f755ed94d6
lijinsong02 <lijinsong02@58.com>
2024-03-25 11:20:12 +0800

    new term can not auto fix window size bug

--- src/pages/Session/SessionTab/Xterminal/sessionWindow.tsx
+++ src/pages/Session/SessionTab/Xterminal/sessionWindow.tsx
@@ -137,138 +137,135 @@ const SessionWindow: React.FC = (props) => {
         }
     };
 
-    useEffect(() => {
-        const searchAddon = new SearchAddon();
-        term.loadAddon(searchAddon);
-        const fitAddon = new FitAddon();
-        term.loadAddon(fitAddon);
-        term._fitAddon = fitAddon;
-        term.open(terminalRef.current as HTMLDivElement);
-        term.focus();
-        window.addEventListener("resize", () => {
-            fitAddon.fit();
-        });
-        fitAddon.fit();
-    }, []);
-
     // 等后端ssh连接建立后再建立websocket连接
     useEffect(() => {
-        const ws_url = util.baseUrl.split(/\?|#/, 1)[0].replace('http', 'ws'),
-            join = (ws_url[ws_url.length - 1] === '/' ? '' : '/'),
-            url = ws_url + join + 'ws?id=' + id,
-            encoding = 'utf-8',
-            decoder = window.TextDecoder ? new window.TextDecoder(encoding) : encoding;
+        if (isConnected) {
+            const searchAddon = new SearchAddon();
+            term.loadAddon(searchAddon);
+            const fitAddon = new FitAddon();
+            term.loadAddon(fitAddon);
+            term._fitAddon = fitAddon;
+            term.open(terminalRef.current as HTMLDivElement);
+            term.focus();
 
-        const sock = new window.WebSocket(url);
-        sessionStatusMap[id] = CONNECTING;
+            const ws_url = util.baseUrl.split(/\?|#/, 1)[0].replace('http', 'ws'),
+                join = (ws_url[ws_url.length - 1] === '/' ? '' : '/'),
+                url = ws_url + join + 'ws?id=' + id,
+                encoding = 'utf-8',
+                decoder = window.TextDecoder ? new window.TextDecoder(encoding) : encoding;
 
-        sock.onopen = function () {
-            // resize_terminal(term);
-            sessionStatusMap[id] = CONNECTED;
-            term._fitAddon.fit();
-        };
+            const sock = new window.WebSocket(url);
+            sessionStatusMap[id] = CONNECTING;
 
-        sessionIdRef[id] = {
-            id: id,
-            sock: sock,
-            term: term,
-            sessionConfId,
-            send: function (msg: object) {
-                sock.send(JSON.stringify(msg));
-            },
-            sendData: function (data: string) {
-                sock.send(JSON.stringify({'data': data, 'type': 'data'}));
-            },
-            sendRecv: async function (data: string, maxRetryCount = 10, retryTime = 1000) {
-                const uid = getUUid();
-                console.log(`uuid: ${uid}`)
+            sock.onopen = function () {
+                // resize_terminal(term);
+                sessionStatusMap[id] = CONNECTED;
+                function sendTermResizeMessage(cols, rows) {
+                    sock.send(JSON.stringify({'type': 'resize', 'resize': [cols, rows]}));
+                }
 
-                sock.send(JSON.stringify({'data': data + '\r', requestId: uid, 'type': 'sendRecv'}));
-                for (let i = 0; i < maxRetryCount; i++) {
-                    console.log('msgMap:', msgMap)
-                    if (uid in msgMap) {
-                        const msg = msgMap[uid];
-                        delete msgMap[uid];
-                        return msg
+                function termResize(size) {
+                    term._fitAddon.fit();
+                    const {cols, rows} = size;
+                    sendTermResizeMessage(cols, rows);
+                }
+
+                term.onResize(termResize);
+
+                window.addEventListener("resize", () => {
+                    fitAddon.fit();
+                });
+
+                term._fitAddon.fit();
+            };
+
+            sessionIdRef[id] = {
+                id: id,
+                sock: sock,
+                term: term,
+                sessionConfId,
+                send: function (msg: object) {
+                    sock.send(JSON.stringify(msg));
+                },
+                sendData: function (data: string) {
+                    sock.send(JSON.stringify({'data': data, 'type': 'data'}));
+                },
+                sendRecv: async function (data: string, maxRetryCount = 10, retryTime = 1000) {
+                    const uid = getUUid();
+                    console.log(`uuid: ${uid}`)
+
+                    sock.send(JSON.stringify({'data': data + '\r', requestId: uid, 'type': 'sendRecv'}));
+                    for (let i = 0; i < maxRetryCount; i++) {
+                        console.log('msgMap:', msgMap)
+                        if (uid in msgMap) {
+                            const msg = msgMap[uid];
+                            delete msgMap[uid];
+                            return msg
+                        }
+                        await sleep(retryTime);
                     }
-                    await sleep(retryTime);
+                    return {
+                        error: 'excced time'
+                    }
                 }
-                return {
-                    error: 'excced time'
-                }
+            };
+
+            sock.onerror = function (e) {
+                console.error(e);
+            };
+
+            term.onData(function (data) {
+                console.log(`onData: ${id}, data: ${data}`);
+                sock.send(JSON.stringify({'data': data, 'type': 'data'}));
+            });
+
+            function term_write(text) {
+                sessionIdRef[id].term.write(text);
             }
-        };
 
-        sock.onerror = function (e) {
-            console.error(e);
-        };
-
-        term.onData(function (data) {
-            console.log(`onData: ${id}, data: ${data}`);
-            sock.send(JSON.stringify({'data': data, 'type': 'data'}));
-        });
-
-        function sendTermResizeMessage(cols, rows) {
-            sock.send(JSON.stringify({'type': 'resize', 'resize': [cols, rows]}));
-        }
-
-        function termResize(size) {
-            term._fitAddon.fit();
-            const {cols, rows} = size;
-            sendTermResizeMessage(cols, rows);
-        }
-
-        term.onResize(termResize);
-
-        function term_write(text) {
-            sessionIdRef[id].term.write(text);
-        }
-
-        function wsockCallback(res) {
-            switch (res.type) {
-                case 'data':
-                    term_write(res.val);
-                    break;
-                case 'message':
-                    showMessage(res);
-                    break;
-                case 'execMethod':
-                    if (res.requestId) {
-                        methodMap[res.method](res.args, (callbackResponse) => {
+            function wsockCallback(res) {
+                switch (res.type) {
+                    case 'data':
+                        term_write(res.val);
+                        break;
+                    case 'message':
+                        showMessage(res);
+                        break;
+                    case 'execMethod':
+                        if (res.requestId) {
+                            methodMap[res.method](res.args, (callbackResponse) => {
+                                sessionIdRef[id].send({
+                                    type: 'callback',
+                                    requestId: res.requestId,
+                                    args: callbackResponse
+                                })
+                            })
+                        } else {
+                            methodMap[res.method](res.args);
+                        }
+                        break;
+                    case 'eval':
+                        const result = eval(`${res.method}('${res.args}')`);
+                        if (res.requestId) {
                             sessionIdRef[id].send({
                                 type: 'callback',
                                 requestId: res.requestId,
-                                args: callbackResponse
+                                args: result
                             })
-                        })
-                    } else {
-                        methodMap[res.method](res.args);
-                    }
-                    break;
-                case 'eval':
-                    const result = eval(`${res.method}('${res.args}')`);
-                    if (res.requestId) {
-                        sessionIdRef[id].send({
-                            type: 'callback',
-                            requestId: res.requestId,
-                            args: result
-                        })
-                    }
-                    break;
-                case 'response':
-                    console.log(res);
-                    if (res.val) {
-                        msgMap[res.requestId] = res.val;
-                    }
-                    console.log(msgMap)
-                    break;
-                default:
-                    throw new Error(`unexpected result type: ${res.type}`);
-                    break;
+                        }
+                        break;
+                    case 'response':
+                        console.log(res);
+                        if (res.val) {
+                            msgMap[res.requestId] = res.val;
+                        }
+                        console.log(msgMap)
+                        break;
+                    default:
+                        throw new Error(`unexpected result type: ${res.type}`);
+                }
             }
-        }
 
-        if (isConnected) {
             sessionIdRef[id].sock.onclose = function (e) {
                 console.log(`sock: ${id} closed`, e);
                 try {
@@ -293,17 +290,16 @@ const SessionWindow: React.FC = (props) => {
                 })
                 // console.log(`sessionIdRef, sessionIdMapFileName, `, sessionIdRef, sessionIdMapFileName)
             };
-        }
 
-        sessionIdRef[id].sock.onmessage = function (msg) {
-            const res = JSON.parse(msg.data);
-            wsockCallback(res);
-        };
+            sessionIdRef[id].sock.onmessage = function (msg) {
+                const res = JSON.parse(msg.data);
+                wsockCallback(res);
+            };
+        }
     }, [isConnected]);
 
-
     return (
-        <div style={{height: `calc(100vh - ${HEADER_HEIGHT}px - 40px)`}} ref={terminalRef}></div>
+        <div style={{height: `calc(100vh - ${HEADER_HEIGHT}px - 40px)`, backgroundColor: 'black'}} ref={terminalRef}></div>
     )
 }
 
