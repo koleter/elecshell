bf1cdf9bf8e212b8298b9297d5e5b32ce1cfcad9
李金松 <lijinsong02@58.com>
2024-11-18 20:05:46 +0800

    refactor(server): 重构配置类并移除未使用的进程工具函数
    - 在 ConfigableGlobalConfig 和 GlobalAutoConfig 类中添加了文档字符串，说明其用途
    - 删除了未使用的 process_util.py 文件，该文件中的函数没有被其他地方调用

--- server/handler/pojo/conf/ConfigableGlobalConfig.py
+++ server/handler/pojo/conf/ConfigableGlobalConfig.py
@@ -5,6 +5,9 @@ import os
 from handler.pojo.conf.CachableConfig import CachableConfig
 
 class ConfigableGlobalConfig(CachableConfig):
+    """
+    a class used by settings
+    """
 
     def __init__(self, path):
         super().__init__(path, "configableGlobalConf.json")
--- server/handler/pojo/conf/GlobalAutoConfig.py
+++ server/handler/pojo/conf/GlobalAutoConfig.py
@@ -5,6 +5,9 @@ import os
 from handler.pojo.conf.CachableConfig import CachableConfig
 
 class GlobalAutoConfig(CachableConfig):
+    """
+    used by some auto save action
+    """
 
     def __init__(self, path):
         super().__init__(path, "autoConf.json")
