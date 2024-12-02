cbfb521ecc1428ea545dd9b827863d9e9da265f4
李金松 <lijinsong02@58.com>
2024-12-02 18:56:04 +0800

    style(Session): 移除会话页面中不必要的定位样式
    移除了 SessionMain 组件外层 div 的 position:'relative' 样式属性，该属性在此处没有实际用途。

--- src/pages/Session/index.tsx
+++ src/pages/Session/index.tsx
@@ -12,7 +12,7 @@ const Session: React.FC = () => {
         <ProjectConfigModal/>
         <div style={{height: '100vh'}}>
             {window.electronAPI.platform == "darwin" && <Header/>}
-            <div style={{position: 'relative', height: `calc(100vh - ${HEADER_HEIGHT}px)`}}>
+            <div style={{height: `calc(100vh - ${HEADER_HEIGHT}px)`}}>
                 <SessionMain></SessionMain>
             </div>
         </div>
