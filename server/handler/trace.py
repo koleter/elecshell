2b7c5fb1172f13550eb300210b4b5a0719c3e061
李金松 <lijinsong02@58.com>
2025-12-18 16:05:40 +0800

    fix(trace): 调整内存快照比较方式并增加显示条目数
    - 将快照比较模式从 'lineno' 改为 'traceback'
    - 将显示的统计条目数量从10条增加到50条
    - 优化内存跟踪信息的详细程度以便更好地分析内存使用情况

--- server/handler/trace.py
+++ server/handler/trace.py
@@ -15,9 +15,9 @@ class TraceHandler(BaseHandler, tornado.web.RequestHandler):
 
     def get(self):
         new_snapshot = tracemalloc.take_snapshot()
-        stats = new_snapshot.compare_to(self.snapshot, 'lineno')
+        stats = new_snapshot.compare_to(self.snapshot, 'traceback')
         ret = ''
-        for stat in stats[:10]:
+        for stat in stats[:50]:
             ret += str(stat) + "\n"
         self.write(ret)
         self.snapshot = new_snapshot
